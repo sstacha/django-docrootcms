a68ebe73b8a7d5c5ec814b2805cc4fbf15925abe
Stephen Stacha <sstacha@gmail.com>
2021-08-12 13:26:47 -0500

    adding index fix for content model for reported bug

--- docrootcms/models.py
+++ docrootcms/models.py
@@ -36,8 +36,8 @@ class ContentMarkdownField(MarkdownxField):
 
 # cms models
 class Content(models.Model):
-    uri = models.CharField(max_length=2000, blank=True)
-    element_id = models.CharField(max_length=500, null=True, blank=True)
+    uri = models.CharField(max_length=700, blank=True)
+    element_id = models.CharField(max_length=150, null=True, blank=True)
     content = models.TextField()
     date_created = models.DateTimeField(auto_now_add=True, editable=False)
     date_modified = models.DateTimeField(auto_now=True, editable=False)
