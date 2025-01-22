a23e0a356777ab8e4af8a14881d5c556b0aa3701
Steve Stacha <sstacha@spe.org>
2025-01-22 16:21:06 -0600

    incrementing setup version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(name='django-docrootcms',
-      version='3.4',
+      version='3.5',
       description='The missing app for developers creating and maintaining websites',
       long_description=long_description,
       long_description_content_type="text/markdown",
